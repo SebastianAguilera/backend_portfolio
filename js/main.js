432c1769c2a7b296cf581b60f5dc6770c9d3ede8
Sebastián Aguilera <sebaalejandro.aguilera@gmail.com>
2025-08-04 20:09:18 -0300

    se agrega menu responsive

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll(".sidebar a");
   const sections = document.querySelectorAll(".section");
+  const toggleButton = document.getElementById("menu-toggle");
+  const sidebar = document.querySelector(".sidebar");
 
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
       const target = link.getAttribute("data-section");
 
-      // Oculta todas las secciones
       sections.forEach((section) => section.classList.remove("active"));
-
-      // Muestra la sección correspondiente
       document.getElementById(target).classList.add("active");
+
+      // Si está en móvil, cerrar menú al hacer clic
+      sidebar.classList.remove("open");
     });
+  });
+
+  toggleButton.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
   });
 });
